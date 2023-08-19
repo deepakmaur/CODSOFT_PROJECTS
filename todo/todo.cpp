fb2cf6e3f60bea2b8f5f13801693c9c439eeb960
deepakmaur <120335053+deepakmaur@users.noreply.github.com>
2023-08-19 18:13:36 +0530

    Update todo.cpp

--- todo/todo.cpp
+++ todo/todo.cpp
@@ -6,8 +6,10 @@ using namespace std;
 string common="Number of task ";
     string progress="Y/N";
     Todo* node1=new Todo(common,progress);
+    Todo* node2=new Todo("Added_Task","Progress");
 
     Todo* tail1=node1;
+    Todo* tail2=node2;
 
 
 
@@ -33,6 +35,28 @@ void addmore (){
                 //goto main;
 }
 
+void addSomemore (){
+    system("CLS");
+    Todo* tail2=node2;
+    cout<<"  Adding task  "<<endl;
+                int n;
+                //string task,prog;
+                cout<<"How many task you wan to add up : ";
+                cin>>n;
+                while(n>0){
+                    string task,prog;
+                    cout<<"Enter task [enter _ for space ]: ";
+                    cin>>task;
+                    cout<<"Enter it's progress : ";
+                    cin>>prog;
+                    addTask(tail2,task,prog);
+                    n--;
+        
+                }
+                system("pause");
+                //goto main;
+}
+
 int main(){
     
     //choice();
@@ -51,9 +75,6 @@ int main(){
     
 
     
-
-    Todo* tail1=node1;
-
     int n;
     cin>>n;
     switch(n){
@@ -61,27 +82,40 @@ int main(){
                 goto jump;
                 break;
 
-        case 2: addmore();
+        case 2: addSomemore();
                 goto jump;
                 break;
 
         case 3: cout<<"    %%%%%   YOUR LIST   %%%%%%%%    "<<endl;
                 print(tail1);
+                print(tail2);
                 goto jump;
                 break;
-        case 4: cout<<"Enter the task you want to delete and its progress : ";
+        case 4: int number;
+                cout<<"Enter [2] if your task was added "<<endl;
+                cin>>number;
+                cout<<"Enter the task you want to delete and its progress : ";
                 string str,p;
                 cin>>str>>p;
-        
-                cout<<"   / DELETING TASK /    ";
-                deleteTask(tail1,str,p);
+
+                if(number==2){
+                    cout<<"   / DELETING TASK /    ";
+                    deleteTask(tail2,str,p);
+
+                }
+                else{
+                    cout<<"   / DELETING TASK /    ";
+                    deleteTask(tail1,str,p);
+                }
                 print(tail1);
+                print(tail2);
+                
                 goto jump;
                 break;
         deafault :
                 cout<<" INVALID ENTRY "<<endl;
                 system("pause");
-                
+                //goto jump;
                 break;
     }
     return 0;
